1760e24d311a26511244507344c66a415138e6e8
gieskiverz <gie.skiverz@gmail.com>
2021-12-08 04:31:02 +0700

    frame19

--- content19.php
+++ content19.php
@@ -48,8 +48,8 @@
                 <div class="bd-example">
                     <ul class="list-group list-group-flush ml-3 mr-2">
                         <li class="list-group-item fw-bold" style="border-bottom: 3px solid !important;">교육제도안내</li>
-                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark active" style="padding: 0rem !important;" href="content18.php">고용보험환급안내</a></a></li>
-                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content19.php">사업주과정안내</a></li>
+                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content18.php">고용보험환급안내</a></a></li>
+                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark active" style="padding: 0rem !important;" href="content19.php">사업주과정안내</a></li>
                         <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content20.php">내일배움카드안내</a></li>
                         <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content21.php">교육생 유의사항</a></li>
                         <li class="list-group-item pt-3 pb-3 border-bottom"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content22.php">훈련진행절차</a></li>
@@ -58,18 +58,24 @@
             </div>
 
             <div class="col-md-10 p-5">
-                <p class="border-top border-bottom border-2 border-dark col-md-2 fs-4 text-center fw-bold">고용보험환급안내</p>
+                <p class="border-top border-bottom border-2 border-dark col-md-2 fs-4 text-center fw-bold">사업주과정안내</p>
                 <p>고용보험 가입 사업주가 인정받은 훈련과정에 대한 훈련비용을 부담하여 재직근로자 등을 대상으로 훈련을 실시한 경우 소정의 훈련비용을 지원</p>
-                <p class="pt-2"><img src="assets/img/dot.png" alt=""> <span class="fw-bold">지원한도</span> (고용보험법 시행령 제42조)</p>
-                <p class="pt-2 f-blue fw-bold">/ 개념 /</p>
-                <p>고용보험 재정의 건전성 및 사업주 훈련 실시의 적정성 확보를 위하여 연간 사업주가 받을 수 있는 직업능력개발 훈련비용의 총액을 제한</p>
-                <p class="pt-2 f-blue fw-bold">/ 지원 한도 금액 /</p>
-                <p class="fw-bold">고용보험법 시행령 제42조제3항 및 사업주훈련 규정 제22조</p>
-                <p><li>사업주가 「보험료징수법」에 따라 해당 연도에 내야 할 고용안정 · 직업능력개발 사업의 개산 보험료 및 특례보험료의 100분의 100</li></p>
-                <p>­­‑ 우선지원대상기업의 경우에는 100분의 240에 해당하는 금액</p>
-                <p>­‑ 비용지원 한도 최소금액(500만원)에 미달하는 경우에는 500만원까지 지원</p>
-                <p>다만, 「고용보험법」제35조에 따라 지원 · 융자 제한의 행정처분을 받은 사업주에 대해서는 그 처분의 효력이 끝나는 보험연도부터 3년간</p>
-                <p class="mb-5">비용 지원한도 최소 금액은 개산 보험료의 240%에 해당하는 금액</p>
+                <p class="pt-2"><img src="assets/img/dot.png" alt=""> <span class="fw-bold">수강신청</span></p>
+                <p class="pt-2 f-blue fw-bold">/ 1 / 교육위탁훈련계약서 작성</p>
+                <p><li>학습을 위해 회원님의 직장에서 '교육위탁훈련계약서'를 작성하신 후 아래와 같이 사본과 원본으로 나눠서 보내주시기 바랍니다.</li></p>
+                <p><li><span style="color: #0897C4;" class="fw-bold">[사본] e-mail</span> : info@ek3434.com</li></p>
+                <p><li><span style="color: #0897C4;" class="fw-bold">[원본] 우편발송</span> : 서울시 금천구 가산디지털1로 219 벽산디지털밸리 6차 303~6호 교육담당자 앞</li></p>
+                <p><li><span style="color: #0897C4;" class="fw-bold">대표전화</span> : 1600-3232</li></p>
+                <p class="col-md-2 offset-md-3"><span class="badge rounded-pill bg-custom-gray pl-2 pr-2 pt-2 pb-2">위탁계약서 다운로드 <img src="assets/img/down.png" alt=""></span></p>
+                <hr>
+                <p class="pt-2 f-blue fw-bold">/ 2 /  위탁계약 체결</p>
+                <p><li>회신하신 위탁계약서 및 위탁교육신청서를 토대로 위탁훈련 계약이 체결됐음을 알려드립니다.</li></p>
+                <p><li>교육원에서 제공해드리는 엑셀 파일 양식에 정보를 입력해서 이메일로 회신해주세요.</li></p>
+                <p><li><span style="color: #0897C4;" class="fw-bold">e-mail</span> : info@ek3434.com</li></p>
+                <hr>
+                <p class="pt-2 f-blue fw-bold">/ 3 /  학습시작</p>
+                <p><li>교육시작일에 맞춰 SMS 및 이메일을 통하여 안내가 나갑니다.</li></p>
+                <hr>
                 <li class="text-center"></li>
                 <li class="text-center"></li>
                 <li class="text-center"></li>
