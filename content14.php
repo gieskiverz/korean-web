bc6e2621592efa9af4f50f1f7455f2ef4fcbf05a
gieskiverz <gie.skiverz@gmail.com>
2021-12-08 03:56:38 +0700

    frame 18

--- content14.php
+++ content14.php
@@ -49,7 +49,7 @@
                     <ul class="list-group list-group-flush ml-3 mr-2">
                         <li class="list-group-item fw-bold" style="border-bottom: 3px solid !important;">교육원소개</li>
                         <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content13.php">CEO 인사말</a></a></li>
-                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-primary" style="padding: 0rem !important;" href="content14.php">비전</a></li>
+                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark active" style="padding: 0rem !important;" href="content14.php">비전</a></li>
                         <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content15.php">조직도</a></li>
                         <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content16.php">연혁</a></li>
                         <li class="list-group-item pt-3 pb-3 border-bottom"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content17.php">오시는길</a></li>
--- content17.php
+++ content17.php
@@ -52,7 +52,7 @@
                         <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content14.php">비전</a></li>
                         <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content15.php">조직도</a></li>
                         <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="content16.php">연혁</a></li>
-                        <li class="list-group-item pt-3 pb-3 border-bottom"><a class="nav-link link-primary" style="padding: 0rem !important;" href="content17.php">오시는길</a></li>
+                        <li class="list-group-item pt-3 pb-3 border-bottom"><a class="nav-link link-dark active" style="padding: 0rem !important;" href="content17.php">오시는길</a></li>
                     </ul>
                 </div>
             </div>
