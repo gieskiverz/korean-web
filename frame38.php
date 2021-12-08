250dea8a25c5cf02bc081ebaf80d159dfaf2068a
gieskiverz <gie.skiverz@gmail.com>
2021-12-09 06:24:06 +0700

    frame 37 38 39

--- frame38.php
+++ frame38.php
@@ -88,8 +88,8 @@
                             </h2>
                             <div id="collapse3" class="accordion-collapse collapse show" aria-labelledby="heading3" data-bs-parent="#accordionExample">
                                 <div class="accordion-body">
-                                    <a class="nav-link link-dark ml-1 mb-2 active" style="padding: 0rem !important;" href="frame37.php">찜한 학습자료</a>
-                                    <a class="nav-link link-dark ml-1 mb-2" style="padding: 0rem !important;" href="frame38.php">1:1 상담 리스트</a>
+                                    <a class="nav-link link-dark ml-1 mb-2" style="padding: 0rem !important;" href="frame37.php">찜한 학습자료</a>
+                                    <a class="nav-link link-dark ml-1 mb-2 active" style="padding: 0rem !important;" href="frame38.php">1:1 상담 리스트</a>
                                     <a class="nav-link link-dark ml-1 mb-2" style="padding: 0rem !important;" href="frame39.php">학습상담리스트</a>
                                 </div>
                             </div>
@@ -123,82 +123,37 @@
             </div>
 
             <div class="col-md-10 pl-6 pr-8">
-                <p class="col-md-2 fs-4 text-start fw-bold pb-5">학습진행과정</p>
-                <div class="container-fluid border border-3 border-light-gray rounded-top pt-3 pb-3">
-                    <div class="row">
-                        <div class="col-md-3"><img src="assets/img/keluargaaneh.png" alt=""></div>
-                        <div class="col-md-6">
-                            <p class="fs-5 fw-bold">NCS기반 어린이집 영유아지원 V1. <img src="assets/img/media.png" alt=""></p>
-                            <p class="fs-6">수강기간 <span class="f-sky-blue">2021.07.15 ~ 2021.07.17</span></p>
-                            <p>내용전문가: 신창길</p>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="row border border-2 border-light-gray rounded">
-                                <div class="col-md-4 border border-1 text-center small p-2">남은 수강일</div>
-                                <div class="col-md-4 border border-1 text-center small p-2">강의진도</div>
-                                <div class="col-md-4 border border-1 text-center small p-2">진도율</div>
-                                <div class="col-md-4 border border-1 text-center p-3 fw-bold"><span class="text-warning">43</span>일</div>
-                                <div class="col-md-4 border border-1 text-center p-3 fw-bold"><span class="text-warning">0</span>/21</div>
-                                <div class="col-md-4 border border-1 text-center p-3 fw-bold"><span class="text-warning">100%</span></div>
-                            </div>
+                <p class="col-md-2 fs-4 text-start fw-bold pb-5">1:1 상담 리스트</p>
+                <div class="row">
+                    <div class="col-md-3 offset-md-9">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="button-addon2">
+                            <button class="btn bg-custom-light-gray" type="button" id="button-addon2"><img src="assets/img/magnify.png" alt=""></button>
                         </div>
                     </div>
                 </div>
-                <div class="accordion" id="accordionExample">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header text-center" id="headingThree">
-                            <button class="collapsed btn bg-custom-light-gray mx-auto d-block w-100" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                <img src="assets/img/sangsakerta.png" alt="">
-                            </button>
-                        </h2>
-                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                            <div class="accordion-body pb-5" style="background-color: #F3F8FA;">
-                                <p class="fw-bold fs-5">학습과정 ❯</p>
-                                <div class="container bg-white rounded">
-                                    <img class="mx-auto d-block" src="assets/img/lobapc.png" alt="">
-                                </div>
-                                <p class="fw-bold fs-5 mt-5">과정차시 ❯</p>
-                                <div class="container bg-white rounded">
-                                    <div class="row">
-                                        <div class="col-md-3 fw-bold fs-5">1차시</div>
-                                        <div class="col-md-3">
-                                            <div class="fw-bold fs-5">문제해결의 전체상의 이해</div>
-                                            <div class="fs-7 text-black-50">최종수강시간: 2021.07.15 15:23:15</div>
-                                            <div class="fs-7 text-black-50">접속 아이피: 59.11.17.119</div>
-                                        </div>
-                                        <div class="col-md-3 fw-bold fs-5">100%</div>
-                                        <div class="col-md-3">
-                                            <button class="btn border border-blue rounded f-blue bg-white">이어보기</button>
-                                            <button class="btn btn-primary">수강하기</button>
-                                        </div>
-                                    </div>
-                                    <hr class="w-100">
-                                    <div class="row">
-                                        <div class="col-md-3 fw-bold fs-5">2차시</div>
-                                        <div class="col-md-3">
-                                            <div class="fw-bold fs-5">문제해결의 전체상의 이해</div>
-                                            <div class="fs-7 text-black-50">최종수강시간: 2021.07.15 15:23:15</div>
-                                            <div class="fs-7 text-black-50">접속 아이피: 59.11.17.119</div>
-                                        </div>
-                                        <div class="col-md-3 fw-bold fs-5">100%</div>
-                                        <div class="col-md-3">
-                                            <button class="btn border border-blue rounded f-blue bg-white">이어보기</button>
-                                            <button class="btn btn-primary">수강하기</button>
-                                        </div>
-                                    </div>
-                                    <hr class="w-100">
-                                    <div class="row">
-                                        <div class="text-center">
-                                            <li class="p-2"></li>
-                                            <li class="p-2"></li>
-                                            <li class="p-2"></li>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+                <table class="table table-borderless">
+                    <thead class="table-secondary border-top border-dark">
+                        <tr class="">
+                            <th scope="col" class="text-center">번호</th>
+                            <th scope="col" class="text-center">문의종류</th>
+                            <th scope="col" class="text-center">제목</th>
+                            <th scope="col" class="text-center">등록일</th>
+                            <th scope="col" class="text-center">답변</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="border-bottom">
+                            <td class="text-center"><span class="pl-2">1</span></td>
+                            <td class="text-center">테스트</td>
+                            <td class="text-center">테스트</td>
+                            <td class="text-center">2021-04-25</td>
+                            <td class="text-center">N</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h5 class="text-center m-5"><span class="btn bg-custom-light-gray rounded-circle">1</span></h5>
             </div>
         </div>
         <!-- end content -->
