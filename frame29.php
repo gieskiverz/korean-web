5ad981ddee94ae5176456cf77538f7aa1ad9427a
gieskiverz <gie.skiverz@gmail.com>
2021-12-08 20:17:28 +0700

    frame 29

---
+++ frame29.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Carousel Template · Bootstrap v5.1</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+    <link href="headers.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include "header.php" ?>
+    <!-- content -->
+    <div class="container-fluid">
+        <div class="row mb-5 mt-5">
+            <div class="col-md-2">
+                <div class="bd-example">
+                    <ul class="list-group list-group-flush ml-3 mr-2">
+                        <li class="list-group-item fw-bold" style="border-bottom: 3px solid !important;">학습지원센터</li>
+                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="frame27.php">공지사항</a></a></li>
+                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="frame28.php">FAQ</a></li>
+                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="frame35.php">자료실</a></li>
+                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark active" style="padding: 0rem !important;" href="frame29.php">PC 원격지원</a></li>
+                        <li class="list-group-item pt-3 pb-3 border-bottom"><a class="nav-link link-dark" style="padding: 0rem !important;" href="frame36.php">교육문의</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-10 pl-6 pr-8">
+                <p class="col-md-2 fs-4 text-start fw-bold pb-5">PC 원격지원</p>
+                <div class="card text-white border-0">
+                    <img src="assets/img/bg-blue.png" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <table class="h-100 w-100">
+                            <tbody>
+                                <tr>
+                                    <td class="align-middle text-center w-50"><img src="assets/img/multipc.png" alt=""></td>
+                                    <td class="align-middle w-50 pr-10">
+                                        <h3 class="fw-bold">원격지원서비스</h3>
+                                        <hr>
+                                        <p>동영상을 보는데 문제가 있으세요?</p>
+                                        <p>아래의 원격지원 프로그램으로 접속하세요.</p>
+                                        <p>저희가 도와드리겠습니다.</p>
+                                        <p class="badge rounded-pill bg-white f-blue p-2 fs-5 mt-5"><span class="ml-2 mr-2">원격지원센터 바로가기</span></p>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end content -->
+        <?php include "footer.php" ?>
+        <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
