110766deab0fb3a374898e884abf72dbe7719815
gieskiverz <gie.skiverz@gmail.com>
2021-12-09 03:01:16 +0700

    frame 26

---
+++ frame26.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Carousel Template · Bootstrap v5.1</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+    <link href="headers.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include "header.php" ?>
+    <!-- content -->
+    <div class="container-fluid">
+        <div class="row mb-5 mt-5">
+            <div class="col-md-2">
+                <div class="bd-example">
+                    <ul class="list-group list-group-flush ml-3 mr-2">
+                        <li class="list-group-item fw-bold" style="border-bottom: 3px solid !important;">법정의무교육</li>
+                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="#">산업공통</a></a></li>
+                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark" style="padding: 0rem !important;" href="#">어린이집 전용</a></li>
+                        <li class="list-group-item pt-3 pb-3"><a class="nav-link link-dark active" style="padding: 0rem !important;" href="frame26.php">의료기관 전용</a></li>
+                        <li class="list-group-item pt-3 pb-3 border-bottom"><a class="nav-link link-dark" style="padding: 0rem !important;" href="frame34.php">요양기관 전용</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-10 pl-6 pr-8">
+                <p class="col-md-2 fs-4 text-start fw-bold pb-5">의료기관 전용</p>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="assets/img/tangan1.png" alt="">
+                            </div>
+                            <div class="col-md-3 ml-1">
+                                <del class="ml-1">0원</del>
+                                <div class="mb-2 fw-bold text-warning">자부담금 0원</div>
+                                <div class="mb-1"><button class="w-75 btn border border-blue rounded f-blue bg-white">미리보기</button></div>
+                                <div class="mb-1"><button class="w-75 btn btn-primary">수강신청</button></div>
+                                <div class="mb-1"><button class="w-75 btn text-black bg-custom-light-gray">문 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;의</button></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="fw-bold fs-5">개인정보보호교육 <img src="assets/img/media.png" alt=""></div>
+                                <div class="fs-7 text-black-50">총 4차시 / 4시간</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="assets/img/tangan2.png" alt="">
+                            </div>
+                            <div class="col-md-3 ml-1">
+                                <del class="ml-1">0원</del>
+                                <div class="mb-2 fw-bold text-warning">자부담금 0원</div>
+                                <div class="mb-1"><button class="w-75 btn border border-blue rounded f-blue bg-white">미리보기</button></div>
+                                <div class="mb-1"><button class="w-75 btn btn-primary">수강신청</button></div>
+                                <div class="mb-1"><button class="w-75 btn text-black bg-custom-light-gray">문 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;의</button></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="fw-bold fs-5">직장 내 성희롱 예방 <img src="assets/img/media.png" alt=""></div>
+                                <div class="fs-7 text-black-50">총 4차시 / 4시간</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="assets/img/tangan1.png" alt="">
+                            </div>
+                            <div class="col-md-3 ml-1">
+                                <del class="ml-1">0원</del>
+                                <div class="mb-2 fw-bold text-warning">자부담금 0원</div>
+                                <div class="mb-1"><button class="w-75 btn border border-blue rounded f-blue bg-white">미리보기</button></div>
+                                <div class="mb-1"><button class="w-75 btn btn-primary">수강신청</button></div>
+                                <div class="mb-1"><button class="w-75 btn text-black bg-custom-light-gray">문 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;의</button></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="fw-bold fs-5">개인정보보호교육 <img src="assets/img/media.png" alt=""></div>
+                                <div class="fs-7 text-black-50">총 4차시 / 4시간</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="assets/img/tangan2.png" alt="">
+                            </div>
+                            <div class="col-md-3 ml-1">
+                                <del class="ml-1">0원</del>
+                                <div class="mb-2 fw-bold text-warning">자부담금 0원</div>
+                                <div class="mb-1"><button class="w-75 btn border border-blue rounded f-blue bg-white">미리보기</button></div>
+                                <div class="mb-1"><button class="w-75 btn btn-primary">수강신청</button></div>
+                                <div class="mb-1"><button class="w-75 btn text-black bg-custom-light-gray">문 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;의</button></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="fw-bold fs-5">직장 내 성희롱 예방 <img src="assets/img/media.png" alt=""></div>
+                                <div class="fs-7 text-black-50">총 4차시 / 4시간</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container mt-5">
+                    <li class="text-center m-3"></li>
+                    <li class="text-center m-3"></li>
+                    <li class="text-center m-3"></li>
+                </div>
+            </div>
+        </div>
+        <!-- end content -->
+        <?php include "footer.php" ?>
+        <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -21,7 +21,7 @@
             </div>
 
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-              <li><a href="#" class="nav-link px-4 link-secondary">법정의무교육</a></li>
+              <li><a href="frame26.php" class="nav-link px-4 link-secondary">법정의무교육</a></li>
               <li><a href="#" class="nav-link px-4 link-dark">직무능력향상교육</a></li>
               <li><a href="frame27.php" class="nav-link px-4 link-dark">학습지원센터</a></li>
               <li><a href="frame46.php" class="nav-link px-4 link-dark">나의 강의실</a></li>
